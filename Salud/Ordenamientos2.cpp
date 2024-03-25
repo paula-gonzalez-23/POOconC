0858422dd9b286260affd604ac8b7e07ff9ed8f7
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-03-25 01:46:55 -0500

    Spotify
    Spotify ya funciona, solo falta ordenar los elementos de las listas y meterle metodos de busqueda

--- Salud/Ordenamientos2.cpp
+++ Salud/Ordenamientos2.cpp
@@ -2,6 +2,106 @@
 #include <vector>
 using namespace std;
 
+//Ordenamiento de array
+
+void ordBur(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1; j++){
+            int temp = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1=temp;]
+        }
+    }
+}
+
+void vectorOrd(int arr[], int tamanio){
+    for(int i=0; i<tamanio; i++){
+        cout<<arr[i]<<" ";
+        cout<< endl;
+    }
+}
+
+int main (){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"El vector original es: "<< " ";
+    vectorOrd(arr, n);
+    ordBur(arr, n);
+    cout<<"El vector ordenado es: "<< " ";
+    vectorOrd(arr, n);
+
+    return 0;
+}
+
+//Ordenamiento de listas
+
+struct Node{
+    int dato;
+    Node* next;
+    Node(int val) : dato(val), next(nullptr){}
+
+};
+
+//Funcion para insertar un nodo al principio de la lista
+
+void insertarInicioLista(Node* &head, int val){
+    Node* nuevoNodo = new Node(val);
+    nuevoNodo->next=head;
+    head = nuevoNodo;
+};
+
+//Funcion para imprimir la lista
+
+void imprimirLista(Node* head){
+    while(head != nullptr){
+        cout << head->dato << " ";
+        head = head->next;
+    }
+    cout << endl;
+};
+
+//Funcion para ordenar la lista
+
+void ordenarLista(Node* &head){
+    if (head==nullptr || head->next==nullptr)
+    return;
+    Node* sorted = nullptr;
+    Node* current = head;
+    while (current != nullptr){
+        Node* nextNode = current->next;
+        if (sorted == nullptr || sorted->dato>=current->dato){ 
+            current->next=sorted;
+            sorted=current;
+        }else {
+            Node* temp = sorted;
+            while (temp->next != nullptr && temp->next->dato < current->dato){
+                temp = temp->next;
+            }
+
+            current->next=temp->next;
+            temp->next=current;
+        }
+    }
+};
+
+int main{
+
+    Node* head = nullptr;
+    
+    insertarInicioLista(head,50);
+    insertarInicioLista(head,160);
+    insertarInicioLista(head,5);
+    insertarInicioLista(head,1);
+
+    cout << "La lista original es: "<<endl;
+    imprimirLista(head);
+    ordenarLista(head);
+    cout << "La lista ordenada es: "<<endl;
+    imprimirLista(head);
+
+    retunr 0;
+
+}
 
 //Poker, crear un vector el usuario ingresa las cartas del poker y se tienen que organizar de mayor a menor y ordenar por pinta (corazon, diamante, picas,treboles)
 
--- Salud/Spotify2.cpp
+++ Salud/Spotify2.cpp
@@ -3,20 +3,177 @@
 #include <algorithm>
 using namespace std;
 
-int main () {
+class ListaEnlazada {
 
-    list<string>listaGeneral;
+    private:
 
-    string nuevaCancion;
+    struct Node {
 
-    cout<<"Ingresa el nombre de las canciones que quieres agregar a la lista general: (pon 0 para salir)"<<endl;
-    cin>>nuevaCancion;
+        string data;
+        Node* next;
 
-    while (nuevaCancion != "0"){
+        Node(string val) : data(val), next(nullptr){}
+    };
 
-        listaGeneral.push_back(nuevaCancion);
-        cin>>nuevaCancion;
+    Node* head;
+
+    public:
+
+    ListaEnlazada() {
+
+        head = nullptr;
     }
+
+    void insertarNodo(string val){
+
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void imprimirLista(){
+
+        Node* current = head;
+        while (current != nullptr){
+            cout<<current->data<<", ";
+            current = current->next;
+        }
+
+        cout << endl;
+    }
+
+    void construirListaEnlazada(const list<string>& listaPaula, const list<string>& listaPaula2){
+        for (auto it = listaPaula2.rbegin(); it != listaPaula2.rend(); ++it){
+            insertarNodo(*it);
+        }
+
+        for (auto it = listaPaula.rbegin(); it != listaPaula.rend(); ++it){
+            insertarNodo(*it);
+        }
+        
+    }
+
+};
+
+class Spotify2 {
+
+    //Atributos
+
+    private:
+    
+    list<string>listaGeneral;
+    list<string>listaPaula;
+    list<string>listaPaula2;
+
+    public:
+
+    //Constructor
+    Spotify2(){
+
+    }
+
+    //Accesores set para modificar y get para mostrar 
+
+    void agregarCancionGeneral(const string& cancion){
+        listaGeneral.push_back(cancion);
+    }
+
+    const list<string>& getlistaGeneral() const{
+        return listaGeneral;
+    }
+
+    void agregarCancionPaula(const string& cancion){
+        listaPaula.push_back(cancion);
+    }
+
+    const list<string>& getlistaPaula() const{
+        return listaPaula;
+    }
+
+    void agregarCancionPaula2(const string& cancion){
+        listaPaula2.push_back(cancion);
+    }
+
+    const list<string>& getlistaPaula2() const{
+        return listaPaula2;
+    }
+
+    //Metodos
+
+    void llenarListaGeneral(){
+        string cancion;
+        cout << "ingrese las canciones para la lista general (escriba 'fin' para terminar)" << endl;
+        while (true){
+            cin >> cancion;
+            if (cancion == "fin"){
+                break;
+            }
+            agregarCancionGeneral(cancion);
+        }
+    }
+
+    void llenarListaPaula(){
+        string cancion;
+        cout << "ingrese las canciones para la lista Paula(escriba 'fin' para terminar)" << endl;
+        while (true){
+            cin >> cancion;
+            if (cancion == "fin"){
+                break;
+            }
+            
+            auto it = find(listaGeneral.begin(), listaGeneral.end(), cancion);
+            if (it != listaGeneral.end()){
+                listaPaula.push_back(cancion);
+                cout << cancion << " - La cancion se ha agregado satisfactoriamente a la lista Paula" << endl;
+            }else {
+                cout << cancion <<" - La cancion no existe en la lista general" << endl;
+                cout <<"Desea agregar la cancion a la lista general?" << endl;
+                cout <<"1. Si" << endl;
+                cout <<"2. No" << endl;
+                int seleccion;
+                cin >>seleccion;
+
+                if (seleccion == 1){
+                    agregarCancionGeneral(cancion);
+                    cout << "La cancion se ha agregado satisfactoriamente a la Lista General" << endl;
+                    cout <<"Siga ingresando las canciones que quiere agregar a la Lista Paula" << endl;
+                }else if (seleccion == 2){
+                    cout << "La cancion no se puede agregar a  Lista Paula porque no existe en Lista General" << endl;
+                }
+            }
+            agregarCancionPaula(cancion);
+        }
+    }
+
+    void llenarListaPaula2(){
+        string cancion;
+        cout << "ingrese las canciones para la lista Paula2 (escriba 'fin' para terminar)" << endl;
+        while (true){
+            cin >> cancion;
+            if (cancion == "fin"){
+                break;
+            }
+            agregarCancionPaula2(cancion);
+        }
+    }
+    
+};
+
+int main() {
+
+    Spotify2 spotify;
+
+    spotify.llenarListaGeneral();
+    spotify.llenarListaPaula();
+    spotify.llenarListaPaula2();
+
+    const list<string>& listaGeneral = spotify.getlistaGeneral();
+    const list<string>& listaPaula = spotify.getlistaPaula();
+    const list<string>& listaPaula2 = spotify.getlistaPaula2();
+
+    ListaEnlazada user1;
+
+    user1.construirListaEnlazada(listaPaula, listaPaula2);
 
     cout<<"La lista General de canciones es: ";
     for (string cancion : listaGeneral){
@@ -24,30 +181,20 @@ int main () {
     }
     cout<<endl;
 
-    list<string>listaPaula;
-
-    string newSong;
-
-    cout<<"Ingresa el nombre de las canciones que quieres agregar a la lista de Paula: (pon 0 para salir)"<<endl;
-    cin>>newSong;
-
-    while (newSong != "0"){
-
-        if (find(listaGeneral.begin(), listaGeneral.end(), newSong) != listaGeneral.end()) {
-            listaPaula.push_back(newSong);
-            cout << newSong <<" - La cancion se ha agregado satisfactoriamente a la Lista Paula" << endl;
-        } else {
-            cout << newSong <<" - La cancion no existe en la lista general" << endl;
-        }
-
-        cin>>newSong;
-    }
-
     cout<<"La lista Paula es: ";
     for (string cancion : listaPaula){
         cout<<cancion<<", ";
     }
     cout<<endl;
+
+    cout<<"La lista Paula2 es: ";
+    for (string cancion : listaPaula2){
+        cout<<cancion<<", ";
+    }
+    cout<<endl;
+
+    cout<< "La lista enlazada es: ";
+    user1.imprimirLista();
 
     return 0;
 }
